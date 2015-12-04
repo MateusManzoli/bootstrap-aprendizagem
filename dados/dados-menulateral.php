5c4cd325dcc294da738e1446a261fba01e21fc2a
Mateus Manzoli <mateus.giacomin2@hotmail.com>
2015-12-04 16:39:41 -0200

    alterei a musica e a pagina do twitter no menu lateral

--- dados/dados-menulateral.php
+++ dados/dados-menulateral.php
@@ -42,22 +42,14 @@ include_once'../../gerenciar/login/gerenciador-login.php';
 
 <div class="video" align="center">
     <video controls="controls" poster="../../imagens/hojeEuToTerrivel.jpg">
-        <source src="../../_media/cristiano.mp4" type="video/mp4"/>
+        <source src="../../_media/cristianoAraujo.mp4" type="video/mp4"/>
     </video>
 </div>
 
 <!-- feed noticias twitter -->
 <?php if (!empty($_SESSION['logado'])) { ?>
-    <a class="twitter-timeline"  href="https://twitter.com/search?q=%40KLM" data-widget-id="666336011782352896">Tweets sobre @KLM</a>
-    <script>!function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-            if (!d.getElementById(id)) {
-                js = d.createElement(s);
-                js.id = id;
-                js.src = p + "://platform.twitter.com/widgets.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }
-        }(document, "script", "twitter-wjs");</script>
-
+                <a class="twitter-timeline"  href="https://twitter.com/search?q=%40avianca" data-widget-id="672845064889049088">Tweets sobre @avianca</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
 <?php } ?>
 </div>
